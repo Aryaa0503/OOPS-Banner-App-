9f5a7bff8e99d5a64b9a090d155a6c1629fe00b3
Aryaa0503 <Ab6840@srmist.edu.in>
2026-02-17 10:58:08 +0530

    UC1 PRintOOPS

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,17 +1,4 @@
-
-/**
- * OOPSBannerApp UC1 – OOPS Banner Display Application
- *
- * This class demonstrates a simple Java application
- * that displays the Object Oriented Programming System
- * acronym OOPS to the console.
- *
- * @author Arya
- * @version 1.0
- */
-
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
         System.out.println("OOPS");
     }
