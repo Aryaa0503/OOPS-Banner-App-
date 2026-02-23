d8b49ca815a9e88ab6d7accc40b7620078832992
Aryaa0503 <Ab6840@srmist.edu.in>
2026-02-23 09:23:32 +0530

    Initial comit -uc4-Array-loops

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,19 @@
-public class HelloWorld{
-    
+public class uc4-Array-loops {
+
+    public static void main(String[] args) {
+
+        // Banner lines stored in String array
+        String[] banner = {
+                String.join(" ", "*****", "*   *", "*****", "*   *", "*   *"),
+                String.join(" ", "*   *", "**  *", "* * *", "*  **", "*   *"),
+                String.join(" ", "*****", "*   *", "*   *", "*   *", "*****"),
+                String.join(" ", "*   *", "*   *", "*   *", "*   *", "*****")
+        };
+
+        // Enhanced for-loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+
+        }
+    }
 }
