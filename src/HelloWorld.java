2ac68b4b3692115d443a2b6df52bce8c2a85b505
Aryaa0503 <Ab6840@srmist.edu.in>
2026-02-24 10:17:21 +0530

    undo the code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,18 +2,5 @@ public class uc4-Array-loops {
 
     public static void main(String[] args) {
 
-        // Banner lines stored in String array
-        String[] banner = {
-                String.join(" ", "*****", "*   *", "*****", "*   *", "*   *"),
-                String.join(" ", "*   *", "**  *", "* * *", "*  **", "*   *"),
-                String.join(" ", "*****", "*   *", "*   *", "*   *", "*****"),
-                String.join(" ", "*   *", "*   *", "*   *", "*   *", "*****")
-        };
-
-        // Enhanced for-loop to print banner
-        for (String line : banner) {
-            System.out.println(line);
-
-        }
     }
 }
