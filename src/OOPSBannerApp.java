2af75a145fe9db3c11ab2e24c5c02bc87caba6e5
Aryaa0503 <Ab6840@srmist.edu.in>
2026-03-01 16:19:23 +0530

    feature/uc6-static-functions

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,18 +1,58 @@
-
-/**
- * OOPSBannerApp UC1 – OOPS Banner Display Application
- *
- * This class demonstrates a simple Java application
- * that displays the Object Oriented Programming System
- * acronym OOPS to the console.
- *
- * @author Arya
- * @version 1.0
- */
-
 public class OOPSBannerApp {
 
+    // Method to generate pattern for letter 'O'
+    public static String[] getOPattern() {
+        return new String[]{
+                "   ***   ",
+                "  *   *  ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                "  *   *  ",
+                "   ***   "
+        };
+    }
+
+    // Method to generate pattern for letter 'P'
+    public static String[] getPPattern() {
+        return new String[]{
+                " ******  ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                " *       ",
+                " *       ",
+                " *       "
+        };
+    }
+
+    // Method to generate pattern for letter 'S'
+    public static String[] getSPattern() {
+        return new String[]{
+                "  *****  ",
+                " *       ",
+                " *       ",
+                "  *****  ",
+                "       * ",
+                "       * ",
+                "  *****  "
+        };
+    }
+
+    // Main method
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        // Call helper methods
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        // Loop-based rendering
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + "  "
+                    + oPattern[i] + "  "
+                    + pPattern[i] + "  "
+                    + sPattern[i]);
+        }
     }
 }
